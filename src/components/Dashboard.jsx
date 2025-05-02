44335d39c49d5904831eae3e5e971c67ca06d691
jensuki <jeni.suk@proton.me>
2025-05-02 12:57:10 -0700

    alphabetically orders

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import axios from 'axios';
 import Spinner from './Spinner';
 
-const stocksToFetch = ['AAPL', 'GOOGL', 'AMZN', 'MSFT', 'TSLA', 'NVDA', 'META', 'NFLX', 'BABA', 'JPM', 'V'];
+const stocksToFetch = ['AAPL', 'AMZN', 'BABA', 'GOOGL', 'JPM', 'META', 'MSFT', 'NFLX', 'NTRA', 'NVDA', 'TSLA', 'V'];
 
 const API_KEY = import.meta.env.VITE_FINNHUB_API_KEY;
 
